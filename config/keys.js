ecb7e9849d1c7c4e1fb9313677ad061cc09ce6d1
rochaksharma348 <64397826+rochaksharma348@users.noreply.github.com>
2022-08-26 18:32:30 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -2,13 +2,13 @@
 
 module.exports = {
     google : {
-        clientId : "957181940918-8fjc73dmlu7men6rv60u4dvi6npgo3r8.apps.googleusercontent.com",
-        clientSecret : "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"
+        clientId : "<url>.apps.googleusercontent.com",
+        clientSecret : "my-secret"
     },
     mongoDB : {
-        dbURI : 'mongodb+srv://rochak:sNRLFABb8gYFuj8X@cluster0.lmofl.mongodb.net/ecommerceapp?retryWrites=true&w=majority'
+        dbURI : 'mongodb+srv://<my-user>:<my-password>@<my-url>?retryWrites=true&w=majority'
     },
     session : {
-        cookieKey : 'rochakisthegreatest'
+        cookieKey : 'rochakssecret'
     }
 }
