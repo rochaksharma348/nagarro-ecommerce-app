098ce18d68c7089b6b8e68e0df163b80ac5d82a5
rochaksharma348 <64397826+rochaksharma348@users.noreply.github.com>
2022-08-26 18:28:50 +0530

    first commit

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+// add to .gitignore
+
+module.exports = {
+    google : {
+        clientId : "957181940918-8fjc73dmlu7men6rv60u4dvi6npgo3r8.apps.googleusercontent.com",
+        clientSecret : "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"
+    },
+    mongoDB : {
+        dbURI : 'mongodb+srv://rochak:sNRLFABb8gYFuj8X@cluster0.lmofl.mongodb.net/ecommerceapp?retryWrites=true&w=majority'
+    },
+    session : {
+        cookieKey : 'rochakisthegreatest'
+    }
+}
